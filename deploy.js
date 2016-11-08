0b7f2139c68600f94431015b6078ca1acf5b0f61
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-11-08 10:23:43 +0100

    fix: handle error cases

--- deploy.js
+++ deploy.js
@@ -1,9 +1,18 @@
 let spawnSync = require('child_process').execSync;
 let spawnOptions = { stdio: 'inherit' };
 
+let spawnIt = (cmd) => {
+  try {
+    spawnSync(cmd, spawnOptions);
+  }
+  catch (e) {
+    //Doesn't feel write to swallow the exceptions but there doesn't September
+    //to be a built in way to supress exceptions if git add and git push fail
+  }
+}
+
 console.log('Getting ready for deployment...hold on!');
-spawnSync(`$(npm bin)/jrp ${__dirname}/_posts`, spawnOptions);
-spawnSync(`git add -A .`, spawnOptions);
-spawnSync(`git commit -m "Adding meta data for related posts"`, spawnOptions);
-spawnSync(`git push origin gh-pages"`, spawnOptions);
+spawnIt(`$(npm bin)/jrp ${__dirname}/_posts`);
+spawnIt(`git add -A . && git commit -m "Adding meta data for related posts"`);
+spawnIt(`git push origin gh-pages`)
 console.log('Everything should be live at http://blog.thoughtram.io');
