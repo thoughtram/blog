44b60dad0ec894a61157decc83c7cf9e35d74e32
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-11-08 10:04:23 +0100

    feat: better deploy script

--- deploy.js
+++ deploy.js
@@ -1,6 +1,9 @@
 let spawnSync = require('child_process').execSync;
 let spawnOptions = { stdio: 'inherit' };
-console.log('foo');
+
+console.log('Getting ready for deployment...hold on!');
 spawnSync(`$(npm bin)/jrp ${__dirname}/_posts`, spawnOptions);
 spawnSync(`git add -A .`, spawnOptions);
 spawnSync(`git commit -m "Adding meta data for related posts"`, spawnOptions);
+spawnSync(`git push origin gh-pages"`, spawnOptions);
+console.log('Everything should be live at http://blog.thoughtram.io');
