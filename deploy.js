250dce11c04adab64b345cc71169e0336f12715b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-03-14 23:40:45 +0100

    chore: activate rest of deploy script

--- deploy.js
+++ deploy.js
@@ -13,24 +13,37 @@ let spawnIt = (cmd) => {
   }
 }
 
-const deployBranch = 'deploy';
+const deployBranch = 'gh-pages';
 const stage1 = 'deploy-stage-1';
 const stage2 = 'deploy-stage-2';
 
-// console.log('Getting ready for deployment...hold on!');
-// spawnIt(`$(npm bin)/jrp ${__dirname}/_posts`);
-// spawnIt(`git add -A . && git commit -m "chore: adds meta data for related posts and videos"`);
-// spawnIt(`git push origin gh-pages`)
-// console.log('Everything should be live at http://blog.thoughtram.io');
+console.log('Getting ready for deployment...hold on!');
 
+// generate meta data for posts
+spawnIt(`$(npm bin)/jrp ${__dirname}/_posts`);
+spawnIt(`git add -A . && git commit -m "chore: adds meta data for related posts and videos"`);
+
+// perform jekyll build
 spawnIt(`jekyll build`);
+
+// cleanup temp branches
 spawnIt(`git branch -D ${stage1}`);
 spawnIt(`git branch -D ${stage2}`);
+
+// prepare stage 1
 spawnIt(`git checkout -b ${stage1}`);
 spawnIt(`git add -f _site`);
 spawnIt(`git commit -m "${stage1}"`);
+
+// prepare stage 2
 spawnIt(`git subtree split -P _site -b ${stage2}`);
+
+// get contents from stage 2 and create a commit
 spawnIt(`git checkout ${deployBranch}`);
 spawnIt(`git checkout ${stage2} .`);
 spawnIt(`git add -A`);
 spawnIt(`git commit -m "rebuilt site"`);
+
+spawnIt(`git push origin ${deployBranch}`);
+
+console.log('Everything should be live at http://blog.thoughtram.io');
