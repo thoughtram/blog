5d2575248be1655ca682bb3c6a6748b55a7a9501
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-03-14 22:57:07 +0100

    chore: prepare build script for new deployment system

--- deploy.js
+++ deploy.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 let spawnSync = require('child_process').execSync;
 let spawnOptions = { stdio: 'inherit' };
 
@@ -11,8 +13,24 @@ let spawnIt = (cmd) => {
   }
 }
 
-console.log('Getting ready for deployment...hold on!');
-spawnIt(`$(npm bin)/jrp ${__dirname}/_posts`);
-spawnIt(`git add -A . && git commit -m "chore: adds meta data for related posts and videos"`);
-spawnIt(`git push origin gh-pages`)
-console.log('Everything should be live at http://blog.thoughtram.io');
+const deployBranch = 'deploy';
+const stage1 = 'deploy-stage-1';
+const stage2 = 'deploy-stage-2';
+
+// console.log('Getting ready for deployment...hold on!');
+// spawnIt(`$(npm bin)/jrp ${__dirname}/_posts`);
+// spawnIt(`git add -A . && git commit -m "chore: adds meta data for related posts and videos"`);
+// spawnIt(`git push origin gh-pages`)
+// console.log('Everything should be live at http://blog.thoughtram.io');
+
+spawnIt(`jekyll build`);
+spawnIt(`git branch -D ${stage1}`);
+spawnIt(`git branch -D ${stage2}`);
+spawnIt(`git checkout -b ${stage1}`);
+spawnIt(`git add -f _site`);
+spawnIt(`git commit -m "${stage1}"`);
+spawnIt(`git subtree split -P _site -b ${stage2}`);
+spawnIt(`git checkout ${deployBranch}`);
+spawnIt(`git checkout ${stage2} .`);
+spawnIt(`git add -A`);
+spawnIt(`git commit -m "rebuilt site"`);
