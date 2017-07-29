c4995a90fff6a6eb97afe8dfd1f99ef9de33a844
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2017-07-29 07:40:42 +0200

    fix: remove sticky script

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,5 @@
     selector: ['h2', 'h3'],
     icon: ''
   });
-
-    var stickyElements = doc.querySelectorAll('.is-sticky');
-
-    for (var i = stickyElements.length - 1; i >= 0; i--) {
-      var el = stickyElements[i];
-
-      if (el.classList.contains('thtrm-toc')) {
-        el.classList.add('thtrm-toc--sticky');
-        el.classList.add('thtrm-toc--faded');
-      } 
-      if (el.classList.contains('thtrm-ad')) {
-        el.classList.add('thtrm-ad--sticky');
-        el.classList.add('thtrm-ad--description');
-      }
-      Stickyfill.add(el);
-    }
+  
 }(document, window));
