6e90deb1bb086fc6391786b53aab1b1f0b7fac62
Pascal Precht <pascal.precht@gmail.com>
2019-09-30 10:58:37 +0200

    WIP SAVEPOINT

--- gatsby/src/components/Header.js
+++ gatsby/src/components/Header.js
@@ -3,39 +3,46 @@ import logo from '../assets/images/logo.svg';
 
 class Header extends React.Component {
 
-  /* constructor() { */
-  /*   super(); */
-  /*   this.state = { */
-  /*     btnVisible */ 
-  /*   } */
-  /* } */
+  constructor() {
+    super();
+    this.menu = React.createRef();
+    this.navToggle = React.createRef();
+  }
 
-  /* toggleNav(btn, state) { */
-  /*   const isVisible = state || */
-  /*     btn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'; */
+  toggleNav(btn) {
+    const isVisible = btn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
 
-  /*   btn.setAttribute( */
-  /*     'aria-expanded', */
-  /*     `${isVisible}` */
-  /*   ); */
-  /*   menu.classList.toggle('is-visible'); */
-  /*   return isVisible; */
-  /* } */
+    btn.setAttribute(
+      'aria-expanded',
+      `${isVisible}`
+    );
+    this.menu.current.classList.toggle('is-visible');
+    return isVisible;
+  }
+
+  hideNav() {
+    this.navToggle.current.setAttribute(
+      'aria-expanded', 'false'
+    );
+    this.menu.current.classList.remove('is-visible');
+  };
 
   handleMenuClick(ev) {
-  /*   const isVisible = btn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'; */
-
-  /*   ev.stopPropagation(); */
-  /*   ev.stopImmediatePropagation(); */
-  /*   console.log('togggle!'); */
-  /*   if (isVisible === 'true') { */
-  /*     body.addEventListener('click', hideNav, {once: true}); */
-  /*   } else { */
-  /*     body.removeEventListener('click', hideNav); */
-  /*   } */
+    const isVisible = this.toggleNav(ev.currentTarget);
+    if (isVisible === 'true') {
+      document.body.addEventListener('click', () => {
+        this.hideNav();
+      }, {once: true});
+    } else {
+      document.body.removeEventListener('click', () => {
+        this.hideNav(0);
+      });
+    }
   }
 
   render() {
+    const menu = this.menu;
+    const navToggle = this.navToggle;
     return (
       <header className="thtrm-banner" role="banner">
         <a href="https://thoughtram.io" title="Home"><img src={logo}
@@ -50,6 +57,7 @@ class Header extends React.Component {
           <button className="thtrm-button--ghost thtrm-nav__button"
                   onClick={(e) => this.handleMenuClick(e)}
                   id="thtrm-nav-button"
+                  ref={navToggle}
                   aria-expanded="false"
                   aria-label="Menu"
                   aria-controls="thtrm-nav-main-list">
@@ -58,7 +66,7 @@ class Header extends React.Component {
                       <svg className="thtrm-icon"><use xlinkHref="#icon--chevron-down"/></svg>
                   </span>
           </button>
-          <ul id="thtrm-nav-main-list" className="thtrm-nav__list">
+          <ul id="thtrm-nav-main-list" ref={menu} className="thtrm-nav__list">
             <li><a title="Training" href="https://thoughtram.io/training.html">Training</a></li>
             <li><a title="Consulting" href="https://thoughtram.io/code-review.html">Consulting</a></li>
             <li><a className="active" title="Blog" href="/">Blog</a></li>
